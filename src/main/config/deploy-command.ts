77adfceae2168dbf3e87a9f128b03e88505580aa
willian <willian.cacoelho@gmail.com>
2024-02-14 23:48:40 -0300

    chore: set up client id for user test bot

--- src/main/config/deploy-command.ts
+++ src/main/config/deploy-command.ts
@@ -7,7 +7,7 @@ export const deployCommand = async () => {
   const commands = slashCommands.map((a) => a.data);
 
   const rest = new REST().setToken(env.token);
-  const clientId = "1207068522430205952";
+  const clientId = env.clientId;
   const guildId = "1201252603191906466";
   const data = await rest
     .put(Routes.applicationGuildCommands(clientId, guildId), { body: commands })
--- src/main/config/env.ts
+++ src/main/config/env.ts
@@ -8,4 +8,5 @@ export const env = {
   port: process.env.PORT || 7070,
   apiUrl: process.env.API_URL || "http://localhost:8080",
   mongoUrl: process.env.MONGO_URL || "mongodb://0.0.0.0:27017/database",
+  clientId: process.env.CLIENT_ID || "1207068522430205952",
 };
